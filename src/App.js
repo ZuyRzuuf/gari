0acd840b96fb286b0d57daa79e5a15195b969aa9
Krzysiek <krzysiek@sobieraj.mobi>
2016-12-17 23:57:12 +0100

    implements redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import gariReducers from './reducers/index';
 import Routes from './components/app/Routes';
 import './App.css';
+
+const store = createStore(gariReducers);
 
 class App extends Component {
     constructor() {
@@ -12,9 +17,11 @@ class App extends Component {
 
     render() {
         return (
-            <MuiThemeProvider >
-                <Routes />
-            </MuiThemeProvider>
+            <Provider store={store}>
+                <MuiThemeProvider >
+                    <Routes />
+                </MuiThemeProvider>
+            </Provider>
         );
     }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+
+const gariReducers = combineReducers({
+
+});
+
+export default gariReducers
