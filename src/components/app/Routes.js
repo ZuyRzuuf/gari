32c6bcc768179dedae5484ea1d4da32d906796e4
Krzysiek Sobieraj <krzysiek@sobieraj.mobi>
2017-01-19 09:26:07 +0100

    remove react-router-redux

--- src/components/app/Routes.js
+++ src/components/app/Routes.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react'
-import { createStore, combineReducers } from 'redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
-import reducers from '../../reducers/index'
 import Layout from './Layout'
 import routes from '../../data/routes'
-
-const store = createStore(
-    combineReducers({
-        ...reducers,
-        routing: routerReducer
-    })
-);
-
-const history = syncHistoryWithStore(browserHistory, store);
 
 class Routes extends Component {
     render() {
         return (
-            <Router history={history}>
+            <Router history={browserHistory}>
                 <Route path='/' component={Layout}>
                     <IndexRoute component={routes.home.component} />
                     <Route path={routes.calendar.path} component={routes.calendar.component} />
